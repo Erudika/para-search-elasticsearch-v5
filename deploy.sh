af6d70a980da6fedce8401787e39c6879341fc39
Alex Bogdanovski <alex@erudika.com>
2021-02-27 15:33:41 +0200

    Release v1.36.1.

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ read -e -p "New dev version: " devver
 
 sed -i -e "s/PARA_PLUGIN_VER=.*/PARA_PLUGIN_VER="\"$ver\""/g" Dockerfile && \
 
-git add -A && git commit -m "Release v$ver." && git push -f origin master && \
-mvn --batch-mode -Dtag=${ver} release:prepare -DreleaseVersion=${ver} -DdevelopmentVersion=${devver}-SNAPSHOT && \
-mvn release:perform && \
-echo "Maven release done, publishing release on GitHub..," && \
+git add -A && git commit -m "Release v$ver." && git push -f origin master #&& \
+#mvn --batch-mode -Dtag=${ver} release:prepare -DreleaseVersion=${ver} -DdevelopmentVersion=${devver}-SNAPSHOT && \
+#mvn release:perform && \
+#echo "Maven release done, publishing release on GitHub..," && \
 echo "v$ver" > changelog.txt && \
 echo "" >> changelog.txt && \
 git log $lastver..HEAD --oneline >> changelog.txt && \
